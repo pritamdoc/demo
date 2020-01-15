7cab45d838f28af393a6fba616708bbf3c0e35ed
Your Name <you@example.com>
2020-01-15 16:22:57 +0530

    edfgewg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject} from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  preserveWhitespaces:false
 })
 export class AppComponent {
-  title = 'sahosoft';
+  title = 'Sahosoft';
+  // constructor(@Inject(MyserviceService)myserviceServicer){
+  //   console.log("myserviceServicer");
+    
+  //   console.log("Hi this is component");
+  // }
+
+  // @HostListener('click',['$event'])
+  // onhostclick(event:Event)
+  // {
+  //   alert('Hello');
+  // }
+
+  btnclick()
+  {
+    alert("You clickesd Me !");
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyModule } from './company/company.module';
+import { MyserviceService } from './myservice.service'
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompanyModule
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
